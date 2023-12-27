024ffd61d60b083e1b333881884752da4d1c82c1
1Seven3Seven <pburgess2003@gmail.com>
2023-12-28 00:50:28 +1100

    Adding them to the package

--- GravitySim/GravitySim.c
+++ GravitySim/GravitySim.c
@@ -1,35 +1,48 @@
 #define PY_SSIZE_T_CLEAN
+
 #include "C:\\Users\\pburg\\AppData\\Local\\Programs\\Python\\Python311\\include\\Python.h"
 
 #include "PyForceAngle.c"
+#include "PyCelestialBody.c"
 
 static PyModuleDef GravitySimModule = {
-    .m_base = PyModuleDef_HEAD_INIT,
-    .m_name = "GravitySim",
-    .m_doc = PyDoc_STR("A library to simulate gravity."),
-    .m_size = -1
+        .m_base = PyModuleDef_HEAD_INIT,
+        .m_name = "GravitySim",
+        .m_doc = PyDoc_STR("A library to simulate gravity."),
+        .m_size = -1
 };
 
 PyMODINIT_FUNC
-PyInit_GravitySim(void) {
-  PyObject *module;
-  
-  if (PyType_Ready(&PyForceAngleType) < 0)
-  {
-    return NULL;
-  }
-  
-  module = PyModule_Create(&GravitySimModule);
-  if (module == NULL)
-  {
-    return NULL;
-  }
-  
-  if (PyModule_AddObjectRef(module, "ForceAngle", (PyObject *) &PyForceAngleType) < 0)
-  {
-    Py_DECREF(module);
-    return NULL;
-  }
-  
-  return module;
+PyInit_GravitySim(void)
+{
+    PyObject * module;
+
+    if (PyType_Ready(&PyForceAngleType) < 0)
+    {
+        return NULL;
+    }
+    if (PyType_Ready(&PyCelestialBodyType) < 0)
+    {
+        return NULL;
+    }
+
+    module = PyModule_Create(&GravitySimModule);
+    if (module == NULL)
+    {
+        return NULL;
+    }
+
+    if (PyModule_AddObjectRef(module, "ForceAngle", (PyObject *)&PyForceAngleType) < 0)
+    {
+        Py_DECREF(module);
+        return NULL;
+    }
+
+    if (PyModule_AddObjectRef(module, "CelestialBody", (PyObject *)&PyCelestialBodyType) < 0)
+    {
+        Py_DECREF(module);
+        return NULL;
+    }
+
+    return module;
 }
