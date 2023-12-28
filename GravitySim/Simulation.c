ab5d4488a085316afd062a11d60583d84452b22c
1Seven3Seven <pburgess2003@gmail.com>
2023-12-29 00:32:28 +1100

    Added a simulation struct to oversee celestial bodies in a simulation.

---
+++ GravitySim/Simulation.c
@@ -0,0 +1,125 @@
+#include "Simulation.h"
+
+#include <stdlib.h>
+
+void simulation_init(Simulation *simulation)
+{
+    simulation->bodies = NULL;
+    simulation->num_bodies = 0;
+
+    simulation->forces = NULL;
+
+    simulation->delta_time = 0;
+}
+
+int simulation_setup(Simulation *simulation, unsigned int number_bodies)
+{
+    unsigned int rows_allocated = 0;
+    simulation->num_bodies = number_bodies;
+
+    // Allocating space for the bodies
+    simulation->bodies = malloc(sizeof(CelestialBody) * number_bodies);
+    if (simulation->bodies == NULL) goto cleanup;
+
+    // Allocating space for pointers to the rows of the ForceAngle matrix
+    // Calloc to make sure each pointer is NULL
+    simulation->forces = calloc(number_bodies, sizeof(ForceAngle *));
+    if (simulation->forces == NULL) goto cleanup;
+
+    // Allocating space for each row of the matrix
+    for (unsigned int i = 0; i < number_bodies; i++)
+    {
+        simulation->forces[i] = malloc(sizeof(ForceAngle) * number_bodies);
+        if (simulation->forces[i] == NULL)
+        {
+            rows_allocated = i;
+            goto cleanup;
+        }
+    }
+
+    return 0;
+
+    // Cleanup performed when a malloc fails
+    cleanup:
+    simulation->num_bodies = 0;
+
+    if (simulation->bodies != NULL)
+    {
+        free(simulation->bodies);
+        simulation->bodies = NULL;
+    }
+
+    if (simulation->forces != NULL)
+    {
+        for (unsigned int i = 0; i < rows_allocated; i++)
+        {
+            free(simulation->forces[i]);
+        }
+
+        free(simulation->forces);
+        simulation->forces = NULL;
+    }
+
+    return -1;
+}
+
+void simulation_de_init(Simulation *simulation)
+{
+    if (simulation->bodies != NULL)
+    {
+        free(simulation->bodies);
+        simulation->bodies = NULL;
+    }
+
+    if (simulation->forces != NULL)
+    {
+        for (unsigned int i = 0; i < simulation->num_bodies; i++)
+        {
+            if (simulation->forces[i] != NULL)
+            {
+                free(simulation->forces[i]);
+                simulation->forces[i] = NULL;
+            }
+        }
+
+        free(simulation->forces);
+        simulation->forces = NULL;
+    }
+
+    simulation->num_bodies = 0;
+}
+
+void simulation_calculate_forces(Simulation *simulation)
+{
+    for (unsigned int i = 0; i < simulation->num_bodies; i++)
+    {
+        for (unsigned int j = 0; j < simulation->num_bodies; j++)
+        {
+            if (i == j) continue;
+
+            simulation->forces[i][j] = calculate_force_between(&simulation->bodies[i], &simulation->bodies[j]);
+        }
+    }
+}
+
+void simulation_apply_forces(Simulation *simulation)
+{
+    for (unsigned int i = 0; i < simulation->num_bodies; i++)
+    {
+        for (unsigned int j = 0; j < simulation->num_bodies; j++)
+        {
+            if (i == j) continue;
+
+            apply_force(&simulation->bodies[i], simulation->forces[i][j]);
+        }
+    }
+}
+
+void simulation_update_positions(Simulation *simulation, double delta_time)
+{
+    for (unsigned int i = 0; i < simulation->num_bodies; i++)
+    {
+        apply_acceleration(&simulation->bodies[i], delta_time);
+        apply_velocity(&simulation->bodies[i], delta_time);
+    }
+}
---
+++ GravitySim/Simulation.h
@@ -0,0 +1,40 @@
+#ifndef GRAVITYSIM_SIMULATION_H
+#define GRAVITYSIM_SIMULATION_H
+
+#include "CelestialBody.h"
+#include "ForceAngle.h"
+
+typedef struct _Simulation
+{
+    /// A dynamically allocated array of celestial bodies in this simulation.
+    CelestialBody *bodies;
+
+    /// The number of bodies in the simulation.
+    unsigned int num_bodies;
+
+    /// A dynamically allocated matrix of the forces between celestial bodies.
+    ForceAngle **forces;
+} Simulation;
+
+/// Initialises the simulation struct.
+/// Must be called before using the struct.
+void simulation_init(Simulation *simulation);
+
+/// Sets up the simulation to handle the given number of celestial bodies.
+/// Returns 0 on success, -1 on failure (a malloc failed).
+/// If a failure occurs then the state is returned to just after simulation_init is called.
+int simulation_setup(Simulation *simulation, unsigned int number_bodies);
+
+/// Frees any allocated memory and returns the state to just after simulation_init is called.
+void simulation_de_init(Simulation *simulation);
+
+/// Calculates the forces acting upon each body according to each other body.
+void simulation_calculate_forces(Simulation *simulation);
+
+/// Applies the forces calculated in simulation_calculate_forces.
+void simulation_apply_forces(Simulation *simulation);
+
+/// Applies the acceleration then the velocity of all bodies to update their positions over some delta time.
+void simulation_update_positions(Simulation *simulation, double delta_time);
+
+#endif
