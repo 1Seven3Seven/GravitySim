27eb7dc88b95ab06b7b38710698ea23a1d2b8243
1Seven3Seven <pburgess2003@gmail.com>
2024-01-09 22:30:00 +1100

    Redoing ForceAngle to be doxygen commented

--- GravitySim/ForceAngle.h
+++ GravitySim/ForceAngle.h
@@ -1,15 +1,22 @@
 #ifndef GRAVITYSIM_FORCEANGLE_H
 #define GRAVITYSIM_FORCEANGLE_H
 
-/// Contains a force and the angle it acts along.
-typedef struct _ForceAngle
+/**
+ * @breif Contains a force and the angle it acts along.
+ */
+typedef struct
 {
     double force;
     double angle;
 } ForceAngle;
 
-inline
-ForceAngle CreateForceAngle(double force, double angle)
+/**
+ * @breif A simple way to create a ForceAngle struct.
+ * @param force The force.
+ * @param angle The angle.
+ * @return The ForceAngle struct.
+ */
+inline ForceAngle CreateForceAngle(double force, double angle)
 {
     ForceAngle force_angle = {force, angle};
     return force_angle;
